f70859ab8d470053f279fc72da073a55828e3dd8
Daniel Pett <dejp3@cam.ac.uk>
2021-04-21 19:04:39 +0100

    First

---
+++ findsbot-tweet.R
@@ -0,0 +1,34 @@
+library(httr)
+library(rtweet)
+library(jsonlite)
+library(digest)
+# Create Twitter token
+findsbot_token <- rtweet::create_token(
+  app = "findsbot",
+  consumer_key =    Sys.getenv("TWITTER_CONSUMER_API_KEY"),
+  consumer_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
+  access_token =    Sys.getenv("TWITTER_ACCESS_TOKEN"),
+  access_secret =   Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+)
+hash <- digest(Sys.time(),algo="md5", serialize=TRUE)
+search <- paste0('https://finds.org.uk/database/search/results/broadperiod/BRONZE+AGE/sort/random_', hash,'/show/1/thumbnail/1/format/json')
+randomFinds <- fromJSON(search)
+id <- randomFinds$results$id
+url <- paste0('https://finds.org.uk/database/artefacts/record/id/', id)
+period <- randomFinds$results$broadperiod
+objectType <- randomFinds$results$objecttype
+county <- randomFinds$results$county
+oldFindID <- randomFinds$results$old_findID
+imagedir <- randomFinds$results$imagedir
+image <- randomFinds$results$filename
+imageUrl <- paste0('https://finds.org.uk/', imagedir, image)
+hashtag <- '#findsorguk #pas'
+tweet <- paste(period,objectType,'from',county,oldFindID,url,hashtag, sep=' ')
+temp_file <- tempfile()
+download.file(imageUrl, temp_file)
+
+rtweet::post_tweet(
+  status = tweet,
+  media = temp_file,
+  token = findsbot_token
+)
