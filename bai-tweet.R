811fcfd7a960234127e93666b65572286e0001be
Daniel Pett <dejp3@cam.ac.uk>
2021-04-21 20:36:07 +0100

    Add bai

---
+++ bai-tweet.R
@@ -0,0 +1,33 @@
+library(httr)
+library(rtweet)
+library(jsonlite)
+library(digest)
+# Create Twitter token
+baibot_token <- rtweet::create_token(
+  app = "findsbot",
+  consumer_key =    Sys.getenv("TWITTER_CONSUMER_API_KEY"),
+  consumer_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
+  access_token =    Sys.getenv("TWITTER_ACCESS_TOKEN"),
+  access_secret =   Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+)
+hash <- digest(Sys.time(),algo="md5", serialize=TRUE)
+search <- paste0('http://131.111.22.71:8983/solr/micropastsBAI/select?q=*%3A*&rows=1&sort=random_',hash,'%20desc')
+randomBAI <- fromJSON(search)
+records <- randomBAI$response$docs
+
+url <- unlist(records$flickrURL)
+period <- 'Bronze Age'
+objectType <- records$objectType
+collection <- unlist(records$collection)
+project <- unlist(records$project)
+imageUrl <- unlist(records$imageURL)
+hashtag <- '#prehistory #bronzeage #bai-bm'
+tweet <- paste(period,objectType,collection,url,hashtag, sep=' ')
+temp_file <- tempfile()
+download.file(imageUrl, temp_file)
+
+rtweet::post_tweet(
+  status = tweet,
+  media = temp_file,
+  token = baibot_token
+)
